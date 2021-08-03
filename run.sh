76f828d3596298a268cd5d31aaac99ac6ae90c9f
xumi1993 <gomijianxu@gmail.com>
2021-08-03 13:56:37 +0800

    update prf

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-cd test/testcase01
-python testcase01.py
-cd ../testcase02
-python testcase02.py
-cd ../..
+sh tc1.sh
---
+++ tc1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+wget https://osf.io/dxcfz/download -O ex-prf.tar.gz
+tar -xzf ex-prf.tar.gz
+cd ex-prf
+
+# TRZ, iter, crust, 0.2
+setpar rf.cfg decon decon_method iter
+setpar rf.cfg save criterion crust
+setpar rf.cfg save rmsgate 0.2
+prf rf.cfg
+
+# TRZ, iter, crust, 
+setpar rf.cfg decon decon_method iter
+setpar rf.cfg save criterion crust
+setpar rf.cfg save rmsgate ""
+prf rf.cfg
+
+# LQT, iter, , 0.2
+setpar rf.cfg rotation lqt
+setpar rf.cfg decon decon_method iter
+setpar rf.cfg save criterion
+setpar rf.cfg save rmsgate 0.2
+prf rf.cfg
+
+# LQT, water, ,
+setpar rf.cfg rotation lqt
+setpar rf.cfg decon decon_method water
+setpar rf.cfg save criterion
+setpar rf.cfg save rmsgate
+prf rf.cfg
+
+# Hk stacking
+hk hk.cfg
+
+cd ..
